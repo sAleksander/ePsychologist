d23dc9e4f1c6fcbb7a93b2c1dda9cf3dd1fec820
GeeA02 <43759867+GeeA02@users.noreply.github.com>
2020-04-22 12:36:50 +0200

    rm comments

--- ePsychologist/Models/Hash.cs
+++ ePsychologist/Models/Hash.cs
@@ -23,37 +23,5 @@ namespace ePsychologist.Models
             byte[] hash = algorithm.ComputeHash(passwordWithSaltBytes);
             return Convert.ToBase64String(hash);
         }
-
-        //public bool tryLogin(string username, string password)
-        //{
-        //    using (var con = new MySqlConnection("host=aaaaaaaa.baaadsg;user=saaaaaak;password=2333333336;database=soaaaaaaaa2;"))
-        //    {
-        //        con.Open();
-
-        //        var salt = string.Empty;
-
-        //        using (var cmd = new MySqlCommand("Select salt From niki where user_name = @username"))
-        //        {
-        //            cmd.Parameters.AddWithValue("@username", username);
-
-        //            salt = cmd.ExecuteScalar() as string;
-        //        }
-
-        //        if (string.IsNullOrEmpty(salt)) return false;
-
-        //        var hashedPassword = GenerateSaltedHash(password, salt);
-
-        //        using (var cmd = new MySqlCommand("Select * FROM niki WHERE user_name = @username and user_password = @password"))
-        //        {
-        //            cmd.Parameters.AddWithValue("@username", username);
-        //            cmd.Parameters.AddWithValue("@password", hashedPassword);
-
-        //            using (var reader = cmd.ExecuteReader())
-        //            {
-        //                return reader.Read();
-        //            }
-        //        }
-        //    }
-        //}
     }
 }
